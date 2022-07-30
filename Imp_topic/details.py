b8a8eb5bbf32f5caed1d94b20cd630993a896a06
DineshKumar9412 <83091156+DineshKumar9412@users.noreply.github.com>
2022-07-30 12:47:12 +0530

    Update details.py

--- Imp_topic/details.py
+++ Imp_topic/details.py
@@ -40,3 +40,55 @@ path_of_the_directory= 'tamil'a = 1for filename in os.listdir(path_of_the_direct
     if os.path.isfile(f):
         sound = AudioSegment.from_file(f)
         a += 1        sound.export(out_f="output/new{}.wav".format(a) ,format="wav", bitrate="128k")
+      
+      
+      
+
+GIT comments
+***************
+git branch -a
+git checkout remotes/origin/Chat_Bot 
+git status
+git commit -m “nnn”
+git push origin HEAD:Chat_Bot 
+git pull
+	
+
+Object Shape 
+**************
+
+https://www.google.com/search?q=opencv+shape+detection+python&ei=epgCYvflJ4G_8QOc6IKIBg&oq=opencv+shape+python&gs_lcp=Cgdnd3Mtd2l6EAMYADIGCAAQBxAeMgYIABAHEB4yBQgAEIAEMggIABAIEAcQHjIICAAQCBAHEB4yCAgAEAgQBxAeMgYIABAIEB4yBggAEAgQHjIGCAAQCBAeMgYIABAIEB46BwgAEEcQsAM6BwgAELADEEM6BwgAELEDEEM6BAgAEEM6BAgAEA1KBAhBGABKBAhGGABQ5wNY-RFg7ypoAXACeACAAWqIAbYEkgEDNS4xmAEAoAEByAEKwAEB&sclient=gws-wiz
+
+
+
+Killer Python Automation Scripts for Daily Problems
+********************************************************
+
+https://python.plainenglish.io/10-python-automation-scripts-for-your-daily-problems-aefb502969e2
+
+https://python.plainenglish.io/9-killer-python-automation-scripts-for-daily-problems-c1715ed42ed5
+
+
+Python BEST website 
+
+https://data-flair.training/blogs/top-python-interview-questions-answer
+
+Python Json
+
+https://medium.com/@alains/python-tutorial-7-advanced-features-of-the-json-library-4b5c485c75bf
+
+Kill Comments  
+***************
+
+sudo lsof -i -P -n | grep LISTEN
+sudo kill -9 738695
+
+
+NOHUP
+********
+
+sudo nohup python Chat_bot_Api.py &
+               
+                      (OR)                
+
+nohup python Chat_bot_Api.py & disown 
