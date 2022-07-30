dd8a2895f25f38573cd86da44c78feefa09d58ac
DineshKumar9412 <83091156+DineshKumar9412@users.noreply.github.com>
2022-07-30 12:58:32 +0530

    Update details.py

--- Imp_topic/details.py
+++ Imp_topic/details.py
@@ -92,3 +92,185 @@ sudo nohup python Chat_bot_Api.py &
                       (OR)                
 
 nohup python Chat_bot_Api.py & disown 
+
+
+
+Chatbot
+************
+
+We have Two Method 
+
+
+First Method
+
+
+1.  https://www.youtube.com/watch?v=9KZwRBg4-P0    
+2. https://github.com/Sourabhhsethii/Building-a-simple-chatbot-in-python
+
+
+Second Method 
+
+1.  https://realpython.com/python-speech-recognition/
+
+
+Video link
+***********
+
+https://www.youtube.com/watch?v=SCk7onPrrwk
+
+	MAP Document
+*************************
+
+https://towardsdatascience.com/make-beautiful-spatial-visualizations-with-plotly-and-mapbox-fd5a638d6b3c
+
+https://towardsdatascience.com/meet-plotly-mapbox-best-choice-for-geographic-data-visualization-599b514bcd9a
+
+Many traces on same plot in plotly express
+
+
+https://stackoverflow.com/questions/60910962/is-there-any-way-to-draw-india-map-in-plotly
+
+
+https://discuss.streamlit.io/t/ann-streamlit-folium-a-component-for-rendering-folium-maps/4367
+
+MAP Document
+*************************
+
+https://towardsdatascience.com/make-beautiful-spatial-visualizations-with-plotly-and-mapbox-fd5a638d6b3c
+
+https://towardsdatascience.com/meet-plotly-mapbox-best-choice-for-geographic-data-visualization-599b514bcd9a
+
+Many traces on same plot in plotly express
+
+
+https://stackoverflow.com/questions/60910962/is-there-any-way-to-draw-india-map-in-plotly
+
+
+https://discuss.streamlit.io/t/ann-streamlit-folium-a-component-for-rendering-folium-maps/4367
+
+	
+Need to learn 
+*********************
+
+
+1. TensorFlow 
+2. Keras
+3. PyTroch
+  4. Scikit-learn Python
+  5. Python Pandas
+  6 . NumPy Python
+
+frame = frame[500:700, 500:1300]
+
+
+Poly Line 
+*********
+
+https://stackoverflow.com/questions/57312028/problem-with-using-cv2-pointpolygontest-and-cv2-polylines
+
+
+CNN MODEL  *************
+
+IMAGE Classification 
+
+https://www.youtube.com/watch?v=ejkRh9obVjk
+
+https://www.youtube.com/watch?v=7MceDfpnP8k
+
+Data Augmentation
+*********************
+
+https://machinelearningmastery.com/how-to-configure-image-data-augmentation-when-training-deep-learning-neural-networks
+
+
+Method 
+
+1. https://www.analyticsvidhya.com/blog/2021/08/image-classification-using-cnn-understanding-computer-vision/
+
+2. https://www.analyticsvidhya.com/blog/2021/06/develop-your-first-image-classification-project-with-convolutional-neural-network/
+
+3. https://www.analyticsvidhya.com/blog/2021/01/image-classification-using-convolutional-neural-networks-a-step-by-step-guide/
+
+
+
+Pre-Training Model
+
+https://www.analyticsvidhya.com/blog/2020/08/top-4-pre-trained-models-for-image-classification-with-python-code/
+
+
+
+Data Science Step 
+*******************
+
+df = pd.read_csv('Iris.csv')
+
+df.info()
+
+df.isnull().sum()
+
+df.shape()
+
+df[‘ibm’] .describe()
+
+df[‘ibm’].fillna(df[‘ibm’].mean())
+
+            Or
+
+df[‘ibm’].dropna(df[‘ibm’].mean())
+
+
+
+
+Dlib_Model
+******************
+
+
+https://programmer.help/blogs/face-recognition-dlib-python-version.html
+
+https://towardsdatascience.com/step-by-step-face-recognition-code-implementation-from-scratch-in-python-cc95fa041120
+
+https://github.com/susantabiswas/FaceRecog
+
+https://www.geeksforgeeks.org/python-multiple-face-recognition-using-dlib/
+
+Face Detector HOG & CNN
+*****************************
+
+https://learnopencv.com/face-detection-opencv-dlib-and-deep-learning-c-python/
+
+https://towardsdatascience.com/cnn-based-face-detector-from-dlib-c3696195e01c
+
+https://livecodestream.dev/post/detecting-face-features-with-python/
+
+
+Folder Looping
+*****************
+
+
+
+for directory in os.listdir('images'):
+    for file in os.listdir(os.path.join('images', directory)):
+        img_path = os.path.join('images', directory, file)
+        img = cv2.imread(img_path)
+GOOGLE COLAB  *****************
+
+
+
+How to unzip file in colab 
+
+1.  !unzip -q "/content/gdrive/My Drive/dataset.zip"
+
+  2.  !tar -xf lfw.tgz
+
+How to Delete folder or file in colab 
+
+https://www.codegrepper.com/code-examples/python/delete+non+empty+directory+python
+
+1. import os
+2. import shutil
+3. os.remove('/your/path/to/file.txt') #removes a file.
+4. os.rmdir('/your/folder/path/') #removes an empty directory.
+5. shutil.rmtree('/your/folder/path/') #deletes a directory and all its contents.
+
+
+
